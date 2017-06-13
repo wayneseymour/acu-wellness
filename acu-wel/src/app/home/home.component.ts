58748270be7d4dc4180e057c32037515954bde28
Wayne Kenneth Seymour III <WayneSeymour@iheartmedia.com>
2017-06-13 10:40:33 -0400

    Fixed up the home comp test.

--- acu-wel/src/app/home/home.component.ts
+++ acu-wel/src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
 
